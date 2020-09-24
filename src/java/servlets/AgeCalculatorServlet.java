c2644fea4791ca89573eb85109225e08ad7607ab
816590 <816590@SAIT225700IT.ACDM.DS.SAIT.CA>
2020-09-24 15:45:42 -0600

    Week 02 Lab Creation Age Calculator completed

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,44 @@
+
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgeCalculatorServlet extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String message;
+        String userage = request.getParameter("age");
+        
+        if(userage == null || userage.equals("")){
+            message = "You must give your current age.";
+        }
+        else{
+            try{
+                int numberAge = Integer.parseInt(userage); 
+                numberAge++;
+                message = "Your age next birthday will be" + numberAge;            
+        }   catch(NumberFormatException e){
+                message = "You must give your current age.";
+            }   
+        }        
+    
+        request.setAttribute("output", message);
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+    }
+
+}
---
+++ web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : ageCalculator
+    Created on : Sep 24, 2020, 2:21:06 PM
+    Author     : 816590
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    
+        <h1>Age Calculator</h1>
+        <form method="post" action="agecalc">
+            <label>Enter your age:</label>
+            <input type="text" name="age" value="${userAge}">
+            <br>
+            <input type="submit" value="Age next birthday ">
+        </form>
+        
+        <p>${output}</p>
+    
+</html>
